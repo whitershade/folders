d45214f3addbc512434e47a98d243ad8805022ad
Alex Prokopenko <alex.r@didww.com>
2018-10-07 05:26:06 +0300

    added postbuild for heroku

--- backend/routes.js
+++ backend/routes.js
@@ -1,19 +1,19 @@
-const api = require('./api');
 const express = require('express');
 const path = require('path');
 const rootPath = require('app-root-path').path;
+const api = require('./api');
 
 
 module.exports = (app) => {
-	app.use('/api', api);
+  app.use('/api', api);
 
-	app.use((err, req, res, next) => {
-		console.error('SOME ERR', err);
-		res.status(400).send({ message: err && err.message ? err.message : err });
-	});
+  app.use((err, req, res, next) => {
+    console.error('SOME ERR', err);
+    res.status(400).send({ message: err && err.message ? err.message : err });
+  });
 
-	app.use(express.static(path.join(rootPath, '..', 'frontend', 'build')));
-	app.get('/*', (req, res) => {
-		res.sendFile(path.join(rootPath, '..', 'frontend', 'build', 'index.html'));
-	});
+  app.use(express.static(path.join(rootPath, '..', 'frontend', 'build')));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(rootPath, '..', 'frontend', 'build', 'index.html'));
+  });
 };
